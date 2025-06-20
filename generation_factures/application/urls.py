c6d390dac5128f7f210fcca379a3b77da7a8fc5b
snyp-prp123 <aksilsadi6000@gmail.com>
2025-06-21 01:41:24 +0200

    Supprime tailwindcss.exe du suivi Git

--- generation_factures/application/urls.py
+++ generation_factures/application/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.acceuil, name='acceuil'),
+    path('', views.accueil, name='accueil'),
+    path('factures/',views.factures, name='factures'),
+    path('produits/', views.produits, name='produits'),
+    path('produits/modifier/<int:id>/', views.modifier_produit, name='modifier_produit'),
+    path('produits/supprimer/<int:id>/', views.supprimer_produit, name='supprimer_produit'),
    
 ]
--- generation_factures/application/views.py
+++ generation_factures/application/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Produits, Factures, FacturesElements
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
-def acceuil(request):
+def accueil(request):
     context = {
         'page_actuel': 'acceuil'
     }
@@ -25,3 +26,46 @@ def produits(request):
 
     return render(request, 'produits.html', context)
 
+def factures(request):
+    # Récupère toutes les factures
+    liste_factures = Factures.objects.all().order_by('id')
+    paginator = Paginator(liste_factures, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'page_actuel': 'factures'
+    }
+
+    return render(request, 'factures.html', context)
+
+def modifier_produit(request, id):
+    produit = get_object_or_404(Produits, id=id)
+
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        prix = request.POST.get('prix')
+        date_peremption = request.POST.get('date_peremption')
+
+        # Met à jour le produit
+        produit.nom = nom
+        produit.prix = prix
+        produit.date_peremption = date_peremption
+        produit.save()
+
+        return redirect('produits') 
+
+    # si le get, on redirige ou on renvoie une erreur (optionnel)
+    return redirect('produits')
+
+def supprimer_produit(request, id):
+    produit = get_object_or_404(Produits, id=id)
+
+    if request.method == 'POST':
+        # Supprime le produit
+        produit.delete()
+        return redirect('produits')
+
+    # si le get, on redirige ou on renvoie une erreur (optionnel)
+    return redirect('produits')
